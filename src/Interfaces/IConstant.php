20a1b89e3dc86591d6ca1478dff336cbc67749af
Amit Joshi <amitjoc@gmail.com>
2026-03-04 09:12:47 +0530

    feat(interface): add interface practice
    added example for interface

---
+++ src/Interfaces/IConstant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App8\Interfaces;
+
+interface IConstant
+{
+
+    /**
+     * Approximation of the number π (pi)
+     *
+     * @var float
+     */
+    public const float PIE = 3.14159265358979323846;
+}
